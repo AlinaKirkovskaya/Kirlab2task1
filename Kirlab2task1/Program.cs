5508c03f216e9131c3ba5764b4695ddd26b94d6f
AlinaKirkovskaya <146915168+AlinaKirkovskaya@users.noreply.github.com>
2023-10-13 11:23:38 +0300

    Add project files.

---
+++ Kirlab2task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirlab2task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = " Lab2task1";
+            double side1, side2;
+            Console.Write("Enter the length of the first side of the rectangle: ");
+            if (!double.TryParse(Console.ReadLine(), out side1))
+            {
+                Console.WriteLine("Please, input correct value in double/float format");
+                return;
+            }
+
+            Console.Write("Enter the length of the second side of the rectangle: ");
+            if (!double.TryParse(Console.ReadLine(), out side2))
+            {
+                Console.WriteLine("Please, input correct value in double/float format");
+                return;
+            }
+
+            Rectangle rect = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Area of ​​the rectangle: {rect.CalculateArea()}");
+            Console.WriteLine($"Perimeter of a rectangle: {rect.CalculatePerimeter()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Kirlab2task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirlab2task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of points for the figure (3-5): ");
+            if (!int.TryParse(Console.ReadLine(), out int numPoints) || numPoints < 3 || numPoints > 5)
+            {
+                Console.WriteLine("Please enter a valid number of points (3-5).");
+                return;
+            }
+
+            List<Point> figurePoints = new List<Point>();
+            for (int i = 0; i < numPoints; i++)
+            {
+                Console.Write($"Enter X coordinate for point {i + 1}: ");
+                if (!double.TryParse(Console.ReadLine(), out double x))
+                {
+                    Console.WriteLine("Please, input correct value in double/float format");
+                    return;
+                }
+
+                Console.Write($"Enter Y coordinate for point {i + 1}: ");
+                if (!double.TryParse(Console.ReadLine(), out double y))
+                {
+                    Console.WriteLine("Please, input correct value in double/float format");
+                    return;
+                }
+
+                Console.Write($"Enter a name for point {i + 1}: ");
+                string name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("Point name cannot be empty.");
+                    return;
+                }
+
+                figurePoints.Add(new Point(x, y, name));
+            }
+
+            Figure figure = new Figure(figurePoints.ToArray());
+            double perimeter = figure.CalculatePerimeter();
+            Console.WriteLine($"Perimeter of the {figurePoints.Count}-sided figure is {perimeter}");
+
+        }
+    }
+    }
+
